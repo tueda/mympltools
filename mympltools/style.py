b1a185027320acbdefc5406007b0259ecec01d8f
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-11-16 19:33:12 +0900

    feat: chain "21.10" to the coloring
    Now `mt.use("21.10")` should give the same coloring with the previous
    version.

--- mympltools/style.py
+++ mympltools/style.py
@@ -1,14 +1,14 @@
 """My Matplotlib styles."""
 import collections.abc
 import sys
-from typing import Any, Dict, Optional, Sequence, Union
+from typing import Any, ChainMap, Dict, Optional, Sequence, Union
 
 import cycler
 import matplotlib.pyplot
 
 __all__ = ("colorblind", "mystyle", "mystyle_21_10", "seaborn_colorblind_10", "use")
 
-Style = Union[str, Dict[str, Any]]
+Style = Union[str, Dict[str, Any], ChainMap[str, Any]]
 
 
 # https://github.com/mwaskom/seaborn/blob/v0.11/seaborn/palettes.py#L40-L41
@@ -29,38 +29,42 @@ seaborn_colorblind_10 = {
     ),
 }
 
-mystyle_21_10 = {
-    "axes.linewidth": 1.2,
-    "font.size": 16,
-    "grid.linestyle": ":",
-    "legend.edgecolor": "0.0",
-    "legend.fancybox": False,
-    "legend.framealpha": 1,
-    "lines.linewidth": 2,
-    "lines.markersize": 6,
-    "xtick.direction": "in",
-    "xtick.major.bottom": True,
-    "xtick.major.size": 5,
-    "xtick.major.top": True,
-    "xtick.major.width": 1,
-    "xtick.minor.bottom": True,
-    "xtick.minor.size": 3,
-    "xtick.minor.top": True,
-    "xtick.minor.visible": True,
-    "xtick.minor.width": 1,
-    "xtick.top": True,
-    "ytick.direction": "in",
-    "ytick.major.left": True,
-    "ytick.major.right": True,
-    "ytick.major.size": 5,
-    "ytick.major.width": 1,
-    "ytick.minor.left": True,
-    "ytick.minor.right": True,
-    "ytick.minor.size": 3,
-    "ytick.minor.visible": True,
-    "ytick.minor.width": 1,
-    "ytick.right": True,
-}
+# style used in a presentation in October 2021
+mystyle_21_10 = ChainMap(
+    seaborn_colorblind_10,
+    {
+        "axes.linewidth": 1.2,
+        "font.size": 16,
+        "grid.linestyle": ":",
+        "legend.edgecolor": "0.0",
+        "legend.fancybox": False,
+        "legend.framealpha": 1,
+        "lines.linewidth": 2,
+        "lines.markersize": 6,
+        "xtick.direction": "in",
+        "xtick.major.bottom": True,
+        "xtick.major.size": 5,
+        "xtick.major.top": True,
+        "xtick.major.width": 1,
+        "xtick.minor.bottom": True,
+        "xtick.minor.size": 3,
+        "xtick.minor.top": True,
+        "xtick.minor.visible": True,
+        "xtick.minor.width": 1,
+        "xtick.top": True,
+        "ytick.direction": "in",
+        "ytick.major.left": True,
+        "ytick.major.right": True,
+        "ytick.major.size": 5,
+        "ytick.major.width": 1,
+        "ytick.minor.left": True,
+        "ytick.minor.right": True,
+        "ytick.minor.size": 3,
+        "ytick.minor.visible": True,
+        "ytick.minor.width": 1,
+        "ytick.right": True,
+    },
+)
 
 colorblind = seaborn_colorblind_10
 mystyle = mystyle_21_10
@@ -82,10 +86,10 @@ def use(styles: Optional[Union[Style, Sequence[Style]]] = None) -> None:
 def _get_style(style: Style) -> Style:
     if isinstance(style, str):
         a = getattr(sys.modules[__name__], f"{style}", None)
-        if isinstance(a, dict):
+        if isinstance(a, (dict, ChainMap)):
             return a
         a = getattr(sys.modules[__name__], f"mystyle_{style.replace('.', '_')}", None)
-        if isinstance(a, dict):
+        if isinstance(a, (dict, ChainMap)):
             return a
         raise ValueError(f"style not found: {style}")
     return style
