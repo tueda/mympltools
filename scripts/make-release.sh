dbd8b777776ab5e94e323f857133b54d49722bea
Takahiro Ueda <tueda@st.seikei.ac.jp>
2022-05-03 17:34:14 +0900

    docs(examples): install the package with the extra "fitting"

--- scripts/make-release.sh
+++ scripts/make-release.sh
@@ -35,7 +35,7 @@ function version_bump() {
   sed -i "s|poetry add $urlbase@.*|poetry add $urlbase@$v$1|" README.md
   sed -i "s|pdm add $urlbase@.*|pdm add $urlbase@$v$1|" README.md
   sed -i "s|Install mympltools .* only when running on|Install mympltools $1 only when running on|" examples/Examples.ipynb
-  sed -i "s|pip install $urlbase@.*\"|pip install $urlbase@$v$1\"|" examples/Examples.ipynb
+  sed -i "s|pip install \\\\\"$urlbase@.*\\\\\"|pip install \\\\\"$urlbase@$v$1#egg=mympltools[fitting]\\\\\"|" examples/Examples.ipynb
   # Check if the files are changed.
   [[ $(numstat README.md) == '3,3' ]]
   [[ $(numstat examples/Examples.ipynb) == '2,2' ]]
